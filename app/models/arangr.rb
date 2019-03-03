1dcc71304b598d7a4d90a2daf0604f8dda7568c5
Cori Caridine <caridine.corin@gmail.com>
2019-03-02 20:40:52 -0600

    index error when data was sent as an array of objects; invalid octal digit

---
+++ app/models/arangr.rb
@@ -0,0 +1,8 @@
+# class Arangr
+#   def self.all
+#     [
+#       { tite: 'SXSW', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCBnCKq0A47VX0n617wfSe7-i2wOJ3K5X0y3RO1svJ1y_mTRj4Yg', date: '2019-03-08', time: 0900, location: 'Austin TX', description: 'music festival of the year', rsvp: true },
+#       { tite: 'SXSW', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCBnCKq0A47VX0n617wfSe7-i2wOJ3K5X0y3RO1svJ1y_mTRj4Yg', date: '2019-03-08', time: 0900, location: 'Austin TX', description: 'music festival of the year', rsvp: true }
+#     ]
+#   end
+# end
