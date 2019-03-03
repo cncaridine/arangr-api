cab0e09022eed49bf66a2578f46aa234ce86b6bd
Cori Caridine <caridine.corin@gmail.com>
2019-03-02 19:33:51 -0600

    created index route and controller

---
+++ app/controllers/arangr_controller.rb
@@ -0,0 +1,27 @@
+class ArangrController < ApplicationController
+  # ===============
+  # index route
+  # ===============
+  def index
+    render json:  { message: 'hi', status: 200 }
+  end
+  # ===============
+  # show route
+  # ===============
+
+  # ===============
+  # create route
+  # ===============
+
+  # ===============
+  # index route
+  # ===============
+
+  # ===============
+  # delete route
+  # ===============
+
+  # ===============
+  # update route
+  # ===============
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ===========
+  # index route
+  # ===========
+  get '/arangr', to: 'arangr#index'
+  # ===========
+  # show route
+  # ===========
+  get '/arangr/:id', to:'arangr#show'
+  # ===========
+  # create route
+  # ===========
+  post '/arangr/:id', to:'arangr#create'
+  # ===========
+  # delete route
+  # ===========
+  delete '/arangr/:id', to:'arangr#delete'
+  # ===========
+  # update route
+  # ===========
+  put '/arangr/:id', to:'arangr#update'
 end
