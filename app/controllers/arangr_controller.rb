7af134fe703451cb441016897812ef6d9f380656
Cori Caridine <caridine.corin@gmail.com>
2019-03-04 07:55:57 -0500

    model created

--- app/controllers/arangr_controller.rb
+++ app/controllers/arangr_controller.rb
@@ -3,10 +3,7 @@ class ArangrController < ApplicationController
   # index route
   # ===============
   def index
-    render json:  [
-      { title: 'SXSW', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCBnCKq0A47VX0n617wfSe7-i2wOJ3K5X0y3RO1svJ1y_mTRj4Yg', date: '2019-03-08', time: 0900, location: 'Austin TX', description: 'music festival of the year', rsvp: true },
-      { title: 'SXSW', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCBnCKq0A47VX0n617wfSe7-i2wOJ3K5X0y3RO1svJ1y_mTRj4Yg', date: '2019-03-08', time: 0900, location: 'Austin TX', description: 'music festival of the year', rsvp: true }
-    ]
+    render json:  Arangr.all
   end
   # ===============
   # show route
--- app/models/arangr.rb
+++ app/models/arangr.rb
@@ -1,8 +1,8 @@
-# class Arangr
-#   def self.all
-#     [
-#       { tite: 'SXSW', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCBnCKq0A47VX0n617wfSe7-i2wOJ3K5X0y3RO1svJ1y_mTRj4Yg', date: '2019-03-08', time: 0900, location: 'Austin TX', description: 'music festival of the year', rsvp: true },
-#       { tite: 'SXSW', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCBnCKq0A47VX0n617wfSe7-i2wOJ3K5X0y3RO1svJ1y_mTRj4Yg', date: '2019-03-08', time: 0900, location: 'Austin TX', description: 'music festival of the year', rsvp: true }
-#     ]
-#   end
-# end
+class Arangr
+  def self.all
+    [
+      { title: 'SXSW', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCBnCKq0A47VX0n617wfSe7-i2wOJ3K5X0y3RO1svJ1y_mTRj4Yg', day: 'march', time: 7, location: 'Austin TX', description: 'music festival of the year', rsvp: true },
+      { title: 'SXSW', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCBnCKq0A47VX0n617wfSe7-i2wOJ3K5X0y3RO1svJ1y_mTRj4Yg', day: 'march', time: 7, location: 'Austin TX', description: 'music festival of the year', rsvp: true }
+    ]
+  end
+end
